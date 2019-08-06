654aa914e82e19079e0640a94ec819d3c0bb6c53
Domas Žičevičius <domas.zicevicius@gmail.com>
2019-08-06 21:00:50 +0300

    Updates

---
+++ precache-manifest.16bd085f265f763bbd1f0be6cd420ac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6ebf3be2c7dea64ca6ebb472d04a8a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c6f9d7f5d9f45b27420a",
+    "url": "/monsters/static/css/main.ba3533bc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c6f9d7f5d9f45b27420a",
+    "url": "/monsters/static/js/main.c9c81df5.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
